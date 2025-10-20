f210b4a1b87f38d38151c66ca7b0e06e8c5fd515
Hamsa Mohammed <72102525+hmoh24@users.noreply.github.com>
2025-10-20 16:49:37 +0100

    Add files via upload

---
+++ hashmap.js
@@ -0,0 +1,260 @@
+function hash(key, capacity) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+  }
+  return hashCode;
+}
+
+class HashMap {
+  #loadFactor;
+  #capacity;
+  #bucket;
+
+  constructor() {
+    this.#capacity = 16;
+    this.#bucket = new Array(this.#capacity);
+    this.#loadFactor = 0.75;
+  }
+
+  static indexOf(key, array) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === undefined) continue;
+      if (Array.isArray(array[i])) {
+        let innerIndex = this.indexOf(key, array[i]);
+        if (innerIndex >= 0) return innerIndex;
+      } else {
+        const [[existingKey, value]] = Object.entries(array[i]);
+        if (existingKey === key) return i;
+      }
+    }
+    return -1;
+  }
+
+  grow(){
+    if (this.length() > (this.#capacity * this.#loadFactor)){
+        this.#capacity *= 2;
+        const oldArray = this.#bucket
+        this.#bucket = new Array(this.#capacity)
+        for (const item of oldArray){
+            if (item === undefined) continue;
+            else if (Array.isArray(item)){
+                item.forEach(object =>{
+                    const [[existingKey, value]] = Object.entries(object);
+                    this.set(existingKey, value)
+                })
+            }
+            else{
+                const [[existingKey, value]] = Object.entries(item);
+                this.set(existingKey, value)
+            }
+        }
+    }
+  }
+
+  set(key, value) {
+    const newObject = { [key]: value };
+    const hashCode = hash(key, this.#capacity);
+    if (hashCode < 0 || hashCode >= this.#capacity) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    const existingValues = this.#bucket[hashCode];
+
+    //add if empty
+    if (existingValues === undefined) {
+      this.#bucket[hashCode] = newObject;
+      this.grow();
+    }
+    //if a single object exists, check if same and replace or turn into array if not same
+    else if (!Array.isArray(existingValues)) {
+      const [[existingKeyName, val]] = Object.entries(existingValues);
+      if (existingKeyName === key)
+        this.#bucket[hashCode][existingKeyName] = value;
+      else {
+        this.#bucket[hashCode] = [existingValues, newObject];
+        this.grow();
+      }
+    }
+    //else if more than 1 (array) exists
+    else {
+      const index = HashMap.indexOf(key, this.#bucket);
+      //push if its unique
+      if (index < 0) {
+        this.#bucket[hashCode] = [...this.#bucket, newObject];
+      }
+      //replace if the same
+      if (index < this.#capacity - 1) {
+        this.#bucket[hashCode][index] = newObject;
+      }
+    }
+  }
+
+  get(key) {
+    let code = hash(key, this.#capacity);
+    if (this.#bucket[code] && Array.isArray(this.#bucket[code])) {
+      for (let i = 0; i < this.#bucket[code].length; i++) {
+        const [[existingKey, value]] = Object.entries(this.#bucket[code][i]);
+        if (existingKey === key) return value;
+      }
+      return null;
+    } else {
+      return this.#bucket[code]?.[key] ?? null;
+    }
+  }
+
+  has(key) {
+    let code = hash(key, this.#capacity);
+    if (this.#bucket[code] && Array.isArray(this.#bucket[code])) {
+      for (let i = 0; i < this.#bucket[code].length; i++) {
+        const [[existingKey, value]] = Object.entries(this.#bucket[code][i]);
+        if (existingKey === key) return true;
+      }
+      return false;
+    } else {
+      return this.#bucket[code]?.[key] ? true : false;
+    }
+  }
+
+  remove(key) {
+    let code = hash(key, this.#capacity);
+
+    if (this.has(key) === false) return false;
+    else {
+      if (Array.isArray(this.#bucket[code])) {
+        let newArray = this.#bucket[code].filter((object) => {
+          let existingKey = Object.keys(object)[0];
+          return existingKey !== key;
+        });
+        this.#bucket[code] = newArray;
+        return true;
+      } else {
+        this.#bucket[code] = undefined;
+        return true;
+      }
+    }
+  }
+
+  length() {
+    let length = 0;
+    for (let i = 0; i < this.#bucket.length; i++) {
+      if (Array.isArray(this.#bucket[i])) {
+        length += this.#bucket[i].length;
+      } else if (this.#bucket[i] !== undefined) {
+        length++;
+      }
+    }
+    return length;
+  }
+
+  clear() {
+    this.#bucket = new Array(16);
+  }
+
+  keys() {
+    let array = [];
+    for (let i = 0; i < this.#bucket.length; i++) {
+      if (Array.isArray(this.#bucket[i])) {
+        for (let j = 0; j < this.#bucket[i].length; j++) {
+          array.push(Object.keys(this.#bucket[i][j])[0]);
+        }
+      } else {
+        if (this.#bucket[i]) {
+            array.push(Object.keys(this.#bucket[i])[0]);
+        }
+        else continue;
+      }
+    }
+    return array;
+  }
+
+  values(){
+    let array = [];
+    for (let i = 0; i < this.#bucket.length; i++) {
+      if (Array.isArray(this.#bucket[i])) {
+        for (let j = 0; j < this.#bucket[i].length; j++) {
+          array.push(Object.values(this.#bucket[i][j])[0]);
+        }
+      } else {
+        if (this.#bucket[i]) {
+            array.push(Object.values(this.#bucket[i])[0]);
+        }
+        else continue;
+      }
+    }
+    return array;
+  }
+
+  entries(){
+    let array = [];
+    for (let i = 0; i < this.#bucket.length; i++) {
+      if (Array.isArray(this.#bucket[i])) {
+        for (let j = 0; j < this.#bucket[i].length; j++) {
+          array.push(Object.entries(this.#bucket[i][j])[0]);
+        }
+      } else {
+        if (this.#bucket[i]) {
+            array.push(Object.entries(this.#bucket[i])[0]);
+        }
+        else continue;
+      }
+    }
+    return array;
+  }
+
+  log() {
+    console.log(this.#bucket);
+    console.log('capacity ', this.#capacity)
+  }
+}
+
+const hashMap1 = new HashMap();
+hashMap1.set("x", "a");
+hashMap1.set("x", "b");
+hashMap1.set("a", "b");
+hashMap1.set("b", "b");
+hashMap1.set("c", "b");
+hashMap1.set("d", "b");
+hashMap1.set("e", "b");
+hashMap1.set("f", "b");
+hashMap1.set("g", "b");
+hashMap1.set("h", "b");
+hashMap1.set("i", "b");
+hashMap1.set("j", "b");
+hashMap1.set("k", "b");
+hashMap1.set("l", "b");
+hashMap1.set("m", "b");
+hashMap1.set("n", "b");
+hashMap1.set("o", "b");
+hashMap1.set("p", "b");
+hashMap1.set("q", "b");
+hashMap1.set("r", "b");
+hashMap1.set("s", "b");
+hashMap1.set("t", "b");
+hashMap1.set("u", "b");
+hashMap1.set("v", "b");
+hashMap1.set("w", "b");
+hashMap1.set("y", "b");
+hashMap1.set("z", "b");
+
+const test = new HashMap();
+ test.set('apple', 'red')
+ test.set('banana', 'yellow')
+ test.set('carrot', 'orange')
+ test.set('dog', 'brown')
+ test.set('elephant', 'gray')
+ test.set('frog', 'green')
+ test.set('grape', 'purple')
+ test.set('hat', 'black')
+ test.set('ice cream', 'white')
+ test.set('jacket', 'blue')
+ test.set('kite', 'pink')
+ test.set('lion', 'golden')
+
+test.set('lion', 'trolden')
+ test.set('kite', 'wink')
+  test.set('moon', 'silver')
+
+ test.log();
+ console.log(test.values());
